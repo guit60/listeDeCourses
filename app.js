7ad12df45cb184245ddf3bd00f5801793ada54f2
guillaumeroberge <guillaume.roberge@interieur.gouv.fr>
2023-02-06 16:54:30 +0100

    init

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const app = express();
 const http = require('http');
 
 
-app.get('/', (req, res) => {
-    res.status(200).send('hello');
-})
+app.set('view engine', 'ejs');
+app.set('views');
 
+app.get('/', (req, res) => {
+    //res.status(200).send('hello');
+    res.status(200).render("accueil");
+});
+app.use((req, res) => {
+    res.status(404).render('page_introuvable');
+});
 
 app.listen('3001', (err) => {
     if (err)

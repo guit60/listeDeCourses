354e1ab52561a2b769ea8efb717b0897240657bc
guillaumeroberge <guillaume.roberge@interieur.gouv.fr>
2023-02-06 17:42:24 +0100

    push pages

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-
+var url = require('url');
 
 app.set('view engine', 'ejs');
 app.set('views');
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     //res.status(200).send('hello');
     res.status(200).render("accueil");
 });
+app.get('/', (req, res) => {
+    let item = url.parse(req.url).query;
+    res.send(item);
+})
 app.use((req, res) => {
     res.status(404).render('page_introuvable');
 });

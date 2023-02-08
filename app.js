bf9be49595fd96b73c2c5f63378b3460a19d9ed7
guillaumeroberge <guillaume.roberge@interieur.gouv.fr>
2023-02-08 17:10:09 +0100

    post form

--- app.js
+++ app.js
@@ -7,14 +7,35 @@ app.set('view engine', 'ejs');
 app.set('views');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+//extraire les liens d'une page web
+// http.get("http://iamjmm.ovh/", function(res) {
+//     console.log(res.statusCode);
+//     res.setEncoding('utf8');
+//     res.on('data', function(data) {
+//         //console.log(data);
+//         let text = data;
+//         let pattern = /href="(.*?)"/g;
+//         let result = text.match(pattern);
+//         console.log(result);
+//     });
+// }).on('error', function(err) {
+//     console.log(err);
+// });
 
 app.get('/', (req, res) => {
     //res.status(200).send('hello');
     res.status(200).render("accueil");
 });
-app.get('/', (req, res) => {
-    let item = url.parse(req.url).query;
-    res.send(item);
+// app.get('/', (req, res) => {
+//     let item = url.parse(req.url).query;
+//     res.send(item);
+// })
+app.post('/notes', (req, res, next) => {
+    let course = req.body.course;
+    console.log(course);
+    next();
 })
 app.use((req, res) => {
     res.status(404).render('page_introuvable');
